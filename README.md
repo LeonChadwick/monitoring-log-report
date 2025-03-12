6a4f702e11d36d935414dd0dbbf1074a0207e229
LeonChadwick <leon@vcto.net>
2025-03-12 18:17:04 +0000

    Checkpoint on progress: Added some structure to parse, type convert and do elapsed time calculation on jobs but still just printing the outcome so far.

--- README.md
+++ README.md
@@ -2,17 +2,22 @@
 An assignment for parsing and reporting based upon logged data.
 
 
+# Open Questions To Enquire On With Stakeholder(s):
+- is the whitespace on the START/STOP status column data guaranteed to be there or should whitespace stripping be implemented?
+- would a job with same description running concurrently but with different pid be a valid scenario or should flag as error if detected?
+
+
+# Description
+
 This application parses its input data collecting statistics on time taken for jobs
 that have logged their run start and end times and reporting those jobs
 that exceed 5 minutes (as a warning) or exceed 10 minutes (as an error).
 
-Log input is expected in CSV format.
+Log input is expected in CSV format and is headerless, i.e. the first row is data just like all the other rows.
 
 Log columns are:
 - HH:MM:SS timestamp
 - Job description
 - enum of {START|END}
 - process id (PID)
-
-
 
--- log_analyser.py
+++ log_analyser.py
@@ -1,14 +1,72 @@
 """ Provide parsing of log content and analytics/reporting from that content """
-from csv import reader
+from csv import DictReader
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
 
 
-def process_log(r: reader):
-    for row in r:
-        print(', '.join(row))
+# Definitions that describe the structure and content of the log file
+class JobStatus(Enum):
+    START = "START"
+    END = "END"
 
+LOG_COLUMN_NAMES = ("timestamp", "description", "status", "pid")
+LOG_TIMESTAMP_FORMAT = "%H:%M:%S"
+
+@dataclass
+class LogLine:
+    timestamp: str
+    description: str
+    status: str
+    pid: str
+
+    def get_key(self):
+        """ Identity of a job run in a log line """
+        return (self.description, self.pid)
+
+    @staticmethod
+    def build(d: dict):
+        d["timestamp"] = datetime.strptime(d["timestamp"], LOG_TIMESTAMP_FORMAT)
+        d["status"] = JobStatus[d["status"]]
+        d["pid"] = int(d["pid"])
+        ll = LogLine(**d)
+        return ll
+
+
+class LogAnalyser:
+    """ Tracking start/stop for jobs via dict, using (description, pid) as the key.
+        TODO: Am making assumption at this time that concurrent runs with same description but different pid is ok, but this needs to be confirmed.
+     """
+
+    def __init__(self):
+        self._runs = {}
+
+    def process_line(self, l: LogLine):
+        if l.status == JobStatus.START:
+            # Record start of a job
+            self._runs[l.get_key()] = l
+        elif l.status == JobStatus.END:
+            # Analyse now job end has been provided
+            prior = self._runs[l.get_key()]
+            if prior is None:
+                # Edge case where log hasn't captured the start time of a job - ignoring
+                pass
+            else:
+                elapsed = l.timestamp - prior.timestamp
+                print(f"elapsed={elapsed}")
+        else:
+            raise ValueError("Unhandled job status")
 
 
 if __name__ == "__main__":
+    analyser = LogAnalyser()
+
+    # we don't read all the content into memory at once which could cause out of memory and if the analysis were
+    # to be run on a production machine could impact memory requirements of production services, so we process as we read each line.
     with open('logs.log') as log_csv_file:
-        r = reader(log_csv_file, delimiter=',', quotechar='"')
-        process_log(r)
+        # skipinitialspace is used here as status column data seems to have whitespace
+        r = DictReader(log_csv_file, fieldnames=LOG_COLUMN_NAMES, delimiter=',', quotechar='"', skipinitialspace=True)
+        for line in r:
+            ll = LogLine.build(line)
+            analyser.process_line(ll)
+
